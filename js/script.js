b47cc58de0ecf9b50891f1cc535407e86249292d
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-04-09 23:19:30 +0530

    assets added and Navbar Made.
    Assets were downloaded using a css pepper extension and navbar is made responsive with respect to the mobile.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const hamButton = document.getElementById("hambtn");
+
+function toggleBar(divToAdd) {
+    const div1 = document.createElement("div")
+    div1.innerHTML = "Pricing"
+
+    const div2 = document.createElement("div")
+    div2.innerHTML = "Docs"
+
+    const div3 = document.createElement("div")
+    div3.innerHTML = "Changelog"
+
+    const div4 = document.createElement("div")
+    div4.innerHTML = "Blog"
+
+    const div5 = document.createElement("div")
+    div5.innerHTML = "Login"
+
+    const Elements = ["Pricing", "Docs", "Changelog", "Blog", "Login"]
+
+    
+
+}
